e6298f3c78a0f5424fb104130f03aad4cd85cc47
yurii-bart <80979377+yurii-bart@users.noreply.github.com>
2021-08-24 09:16:12 +0300

    Feature/3-index-valuation-explorer (#37)
    * Added GetResult extension method for ActionResult<T>
    
    * Added tests
    
    * fixed email signature

---
+++ src/Trakx.Utils/Extensions/ActionResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trakx.Utils.Extensions
+{
+    public static class ActionResultExtensions
+    {
+        public static T GetResult<T>(this ActionResult<T> actionResult)
+        {
+            return (T)((OkObjectResult) actionResult.Result).Value;
+        }
+    }
+}
