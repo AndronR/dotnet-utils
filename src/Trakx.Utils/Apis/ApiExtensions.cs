11da6ce486e576b7a7f413e793515b606c6b0a59
Yurii Bartish <yurii@trakx.io>
2021-08-30 14:46:42 +0300

    Added api versioning.

---
+++ src/Trakx.Utils/Apis/ApiExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Trakx.Utils.Apis
+{
+    public static class ApiExtensions
+    {
+        public static void AddVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(config =>
+            {
+                var version = Assembly.GetEntryAssembly()?.GetName().Version!;
+                config.DefaultApiVersion = new ApiVersion(version.Major, version.Minor);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+
+                config.ApiVersionReader = ApiVersionReader.Combine(
+                    new HeaderApiVersionReader("x-api-version"),
+                    new MediaTypeApiVersionReader("x-api-version")
+                );
+            });
+        }
+    }
+}
