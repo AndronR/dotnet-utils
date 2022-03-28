d1fae2d2db2356aecdfc9a9a91dc873380126f30
monsieurleberre <monsieur.leberre@gmail.com>
2022-03-28 16:07:52 +0100

    add caching package This package should then be used as a place where an app can refer to build keys and retrieve objects it needs

---
+++ src/Trakx.Utils.Caching/AddDistributedCacheExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Trakx.Utils.Caching;
+
+public static class AddDistributedCacheExtension
+{
+    public static void AddDistributedCache(this IServiceCollection services, IConfiguration configuration)
+    {
+        var redisConfig = configuration
+            .GetSection(nameof(RedisCacheConfiguration)).Get<RedisCacheConfiguration>();
+        services.AddSingleton(redisConfig);
+        services.AddDistributedRedisCache(options =>
+        {
+            options.Configuration = redisConfig.ConnectionString;
+        });
+    }
+}
