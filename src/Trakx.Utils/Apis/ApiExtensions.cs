9c1a760fa269514dda354eade199434f268737ac
monsieurleberre <monsieur.leberre@gmail.com>
2022-03-30 08:54:00 +0100

    Add logging static method for faulty API responses

--- src/Trakx.Utils/Apis/ApiExtensions.cs
+++ src/Trakx.Utils/Apis/ApiExtensions.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
 
 namespace Trakx.Utils.Apis;
 
@@ -11,7 +14,7 @@ public static class ApiExtensions
         var apiVersion = ApiVersion.Parse(version);
         AddVersioning(services, apiVersion);
     }
-        
+
     public static void AddVersioning(this IServiceCollection services, ApiVersion apiVersion)
     {
         services.AddApiVersioning(config =>
@@ -24,5 +27,21 @@ public static class ApiExtensions
                 new HeaderApiVersionReader("x-api-version")
             );
         });
+    }
+
+    public static void LogApiFailure(this Serilog.ILogger logger, DelegateResult<HttpResponseMessage> result, TimeSpan timeSpan, int retryCount, Context context)
+    {
+         if (result.Exception != null)
+         {
+             logger.Warning(result.Exception, "An exception occurred on retry {RetryAttempt} for {PolicyKey} - Retrying in {SleepDuration}ms",
+                 retryCount, context.PolicyKey, timeSpan.TotalMilliseconds);
+         }
+         else
+         {
+             logger.Warning("A non success code {StatusCode} with reason {Reason} and content {Content} was received on retry {RetryAttempt} for {PolicyKey} - Retrying in {SleepDuration}ms",
+                 (int)result.Result.StatusCode, result.Result.ReasonPhrase,
+                 result.Result.Content?.ReadAsStringAsync().GetAwaiter().GetResult(),
+                 retryCount, context.PolicyKey, timeSpan.TotalMilliseconds);
+         }
     }
 }
